7eb4a54713899ad50b42e21f2e793d705dbc8f8d
Dafa Shanrizki <dafashanrizki2200@gmail.com>
2025-06-19 10:10:48 +0700

    Top Tab Navigator

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import HomeScreen from "./src/components/HomeScreen";
 import SettingScreen from "./src/components/SettingScreen";
+import Foods from "./src/components/Foods";
+import Drinks from "./src/components/Drinks";
 
-const Tab = createBottomTabNavigator();
+const TopTab = createMaterialTopTabNavigator();
+const ButtonTab = createBottomTabNavigator();
+
+const TopTabNavigator = () => {
+  return (
+    <TopTab.Navigator>
+      <TopTab.Screen name="Food" component={Foods} />
+      <TopTab.Screen name="Drink" component={Drinks} />
+    </TopTab.Navigator>
+  );
+};
 
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator
+    <ButtonTab.Navigator
       screenOptions={{
         tabBarLabelPosition: "beside-icon",
         tabBarLabelStyle: {
@@ -18,9 +31,10 @@ function BottomTabNavigator() {
         tabBarIconStyle: { display: "none" },
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingScreen} />
-    </Tab.Navigator>
+      <ButtonTab.Screen name="Home" component={HomeScreen} />
+      <ButtonTab.Screen name="Settings" component={SettingScreen} />
+      <ButtonTab.Screen name="Product" component={TopTabNavigator} />
+    </ButtonTab.Navigator>
   );
 }
 
---
+++ src/components/Drinks.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Drinks = () => {
+  return (
+    <View>
+      <Text>Drinks</Text>
+    </View>
+  );
+};
+
+export default Drinks;
