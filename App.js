dab997b7dd53168223307babfcb0de8e3d5334f2
Dafa Shanrizki <dafashanrizki2200@gmail.com>
2025-06-19 09:27:37 +0700

    section list

--- App.js
+++ App.js
@@ -1,80 +1,123 @@
-import React, { useState } from "react";
-import {
-  FlatList,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, SectionList, StatusBar } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 const DATA = [
   {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
+    title: "Main dishes",
+    data: ["🍕", "🍔", "🥘"],
   },
   {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
+    title: "Sides",
+    data: ["🍟", "🧅", "🍤"],
   },
   {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
+    title: "Drinks",
+    data: ["🍾", "🥤", "🍺"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
   },
 ];
 
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity
-    onPress={onPress}
-    style={[styles.item, { backgroundColor }]}
-  >
-    <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
-  </TouchableOpacity>
+// const DATA = [
+//   {
+//     title: "The Shawshank Redemption",
+//     year: 1994,
+//     director: "Frank Darabont",
+//     genres: ["Drama", "Crime"],
+//     rating: 9.3,
+//     image_url: "https://example.com/shawshank_redemption.jpg",
+//   },
+//   {
+//     title: "The Godfather",
+//     year: 1972,
+//     director: "Francis Ford Coppola",
+//     genres: ["Crime", "Drama"],
+//     rating: 9.2,
+//     image_url: "https://example.com/the_godfather.jpg",
+//   },
+//   {
+//     title: "Pulp Fiction",
+//     year: 1994,
+//     director: "Quentin Tarantino",
+//     genres: ["Crime", "Drama"],
+//     rating: 8.9,
+//     image_url: "https://example.com/pulp_fiction.jpg",
+//   },
+//   {
+//     title: "The Dark Knight",
+//     year: 2008,
+//     director: "Christopher Nolan",
+//     genres: ["Action", "Crime", "Drama"],
+//     rating: 9.0,
+//     image_url: "https://example.com/the_dark_knight.jpg",
+//   },
+//   {
+//     title: "Inception",
+//     year: 2010,
+//     director: "Christopher Nolan",
+//     genres: ["Action", "Sci-Fi", "Adventure"],
+//     rating: 8.8,
+//     image_url: "https://example.com/inception.jpg",
+//   },
+// ];
+
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container} edges={["top"]}>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  </SafeAreaProvider>
 );
 
-const App = () => {
-  const [selectedId, setSelectedId] = useState();
+const reducedData = DATA.reduce((acc, { title, data }) => {
+  acc[title] = data;
+  return acc;
+}, {});
 
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-    const color = item.id === selectedId ? "white" : "black";
-
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={backgroundColor}
-        textColor={color}
-      />
-    );
-  };
-
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          extraData={selectedId}
-        />
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
-};
+console.log(reducedData);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+    backgroundColor: "#f0f0f0",
   },
   item: {
+    backgroundColor: "#fafafa",
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff",
+    fontWeight: "bold",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
   },
   title: {
-    fontSize: 32,
+    fontSize: 24,
+    color: "#333",
   },
 });
 
